ebf7305bc31e1203cb3352824f2f276d1cda3c78
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2024-10-09 13:30:28 -0400

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -4,7 +4,7 @@ echo "------------------Task a-------------------------------"
 grep -r -l 'sample' ./dataset1/ | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then echo "{}"; fi'
 
 echo "------------------Task b-------------------------------"
-grep -r -l '' ./dataset1/ | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); echo "{}=$count"' | gawk -F= '$2 >= 0 {print $1"," "count="$2}' | sort -k2,5nr
+grep -r -l '' ./dataset1/ | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); echo "{}=$count"' | gawk -F= '$2 >= 0 {print $1"," "count="$2}' | sort -t '=' -k2 -nr
 
 echo "------------------Task c-------------------------------"
 grep -r -l '' ./dataset1/ |  sed 's/file_/filtered_/' 
