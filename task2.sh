5d32687a2f4d43f04bbacb2fc22c3266ffda49d0
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2024-10-09 16:17:20 -0400

    Single pipeline command for task 2

--- task2.sh
+++ task2.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-echo "------------------Task a-------------------------------"
-grep -r -l 'sample' ./dataset1/ | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then echo "{}"; fi'
-
-echo "------------------Task b-------------------------------"
-grep -r -l '' ./dataset1/ | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); echo "{}=$count"' | gawk -F= '$2 >= 0 {print $1"," "count="$2}' | sort -t '=' -k2 -nr
-
-echo "------------------Task c-------------------------------"
-grep -r -l '' ./dataset1/ |  sed 's/file_/filtered_/' 
- 
+echo "------------------Task 2-------------------------------"
+grep -r -l 'sample' ./dataset1/ | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then echo "{}"; fi' | xargs -I {} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); echo "{}=$count"' | gawk -F= '$2 >= 0 {print $1"," "count="$2}' | sort -t'=' -k2 -nr  |  sed 's/file_/filtered_/' 
